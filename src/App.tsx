f5e127659c3aaac1a47ebd8c42029a95c77e70ae
Shriharsha KL <klshriharsha@gmail.com>
2022-10-01 12:33:21 +0530

    feat: add run button - add a non-functional run button - setup manifest json for extension - setup formatter and linters

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './app.css'
+
+function App() {
+    return (
+        <div className="container">
+            <button className="btn">Run</button>
+        </div>
+    )
+}
+
+export default App
